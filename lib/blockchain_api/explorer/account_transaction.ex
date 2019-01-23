8636ee40ef7a7a65d92038849a59f39f0c84c936
Rahul Garg <vihu89@gmail.com>
2019-01-23 13:21:16 -0800

    Composite primary key for account_transactions

--- lib/blockchain_api/explorer/account_transaction.ex
+++ lib/blockchain_api/explorer/account_transaction.ex
@@ -2,9 +2,10 @@ defmodule BlockchainAPI.Explorer.AccountTransaction do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key false
   schema "account_transactions" do
-    field :account_address, :string
-    field :txn_hash, :string
+    field :account_address, :string, primary_key: true
+    field :txn_hash, :string, primary_key: true
 
     belongs_to :account, BlockchainAPI.Explorer.Account, foreign_key: :address, references: :address, define_field: false
     belongs_to :transaction, BlockchainAPI.Explorer.Transaction, foreign_key: :hash, references: :hash, define_field: false
--- priv/repo/migrations/20190122053407_add_account_transaction_table.exs
+++ priv/repo/migrations/20190122053407_add_account_transaction_table.exs
@@ -2,13 +2,17 @@ defmodule BlockchainAPI.Repo.Migrations.AddAccountTransactionTable do
   use Ecto.Migration
 
   def change do
-    create table(:account_transactions) do
+    create table(:account_transactions, primary_key: false) do
       add :account_address, references(:accounts, on_delete: :nothing, column: :address, type: :string), null: false
       add :txn_hash, references(:transactions, on_delete: :nothing, column: :hash, type: :string), null: false
 
       timestamps()
     end
 
-    create unique_index(:account_transactions, [:account_address, :txn_hash], name: :unique_account_txn_hash)
+    alter table(:account_transactions, primary_key: false) do
+      modify :account_address, :string, primary_key: true
+      modify :txn_hash, :string,  primary_key: true
+    end
+
   end
 end
