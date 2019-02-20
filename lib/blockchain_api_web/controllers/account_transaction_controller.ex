8a4884963b07ad258b949b71977751ae7ef42136
Rahul Garg <vihu89@gmail.com>
2019-02-20 12:03:04 -0800

    Simplify account transaction controller

--- lib/blockchain_api_web/controllers/account_transaction_controller.ex
+++ lib/blockchain_api_web/controllers/account_transaction_controller.ex
@@ -3,27 +3,12 @@ defmodule BlockchainAPIWeb.AccountTransactionController do
 
   alias BlockchainAPI.Explorer
   require Logger
-  @default_params %{page: 1, page_size: 10}
 
   action_fallback BlockchainAPIWeb.FallbackController
 
-  def index(conn, %{"account_address" => address}) do
+  def index(conn, %{"account_address" => address}=params) do
 
-    page = Explorer.get_account_transactions(address, @default_params)
-
-    render(conn,
-      "index.json",
-      account_transactions: page.entries,
-      page_number: page.page_number,
-      page_size: page.page_size,
-      total_pages: page.total_pages,
-      total_entries: page.total_entries
-    )
-  end
-
-  def index(conn, %{"account_address" => address, "page" => page, "page_size" => page_size}) do
-
-    page = Explorer.get_account_transactions(address, %{page: String.to_integer(page), page_size: String.to_integer(page_size)})
+    page = Explorer.get_account_transactions(address, params)
 
     render(conn,
       "index.json",
@@ -34,5 +19,4 @@ defmodule BlockchainAPIWeb.AccountTransactionController do
       total_entries: page.total_entries
     )
   end
-
 end
