aeb9692acfd22d672340a176695c0e0bc825c4ca
Rahul Garg <vihu89@gmail.com>
2019-02-24 11:16:26 -0800

    Fix bad sql join for pending transactions

--- lib/blockchain_api_web/controllers/account_pending_transaction_controller.ex
+++ lib/blockchain_api_web/controllers/account_pending_transaction_controller.ex
@@ -6,17 +6,10 @@ defmodule BlockchainAPIWeb.AccountPendingTransactionController do
 
   action_fallback BlockchainAPIWeb.FallbackController
 
-  def index(conn, %{"account_address" => address}=params) do
-
-    page = Explorer.get_account_pending_transactions(address, params)
-
+  def index(conn, %{"account_address" => address}=_params) do
     render(conn,
       "index.json",
-      account_pending_transactions: page.entries,
-      page_number: page.page_number,
-      page_size: page.page_size,
-      total_pages: page.total_pages,
-      total_entries: page.total_entries
+      account_pending_transactions: Explorer.get_account_pending_transactions(address)
     )
   end
 
