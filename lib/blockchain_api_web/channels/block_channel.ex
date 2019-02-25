23f8f5f99631a5987068f0eed95796b9dc39b6c1
Rahul Garg <vihu89@gmail.com>
2019-02-25 15:42:06 -0800

    Fix block payload

--- lib/blockchain_api_web/channels/block_channel.ex
+++ lib/blockchain_api_web/channels/block_channel.ex
@@ -1,18 +1,13 @@
 defmodule BlockchainAPIWeb.BlockChannel do
   use BlockchainAPIWeb, :channel
+  alias BlockchainAPI.Explorer.Block
 
   def join("block:update", _params, socket) do
     {:ok, %{data: "joined block:update"}, socket}
   end
 
   def broadcast_change(block) do
-    payload = %{
-      height: block.height,
-      hash: block.hash,
-      round: block.round,
-      time: block.time
-    }
-
+    payload = block |> Block.encode_model()
     BlockchainAPIWeb.Endpoint.broadcast("block:update", "change", payload)
   end
 
