30671187b0b54b22350394180836e426e1632422
Rahul Garg <vihu89@gmail.com>
2019-03-04 16:53:05 -0800

    Add account with balance to account_controller

--- lib/blockchain_api_web/controllers/account_controller.ex
+++ lib/blockchain_api_web/controllers/account_controller.ex
@@ -1,7 +1,7 @@
 defmodule BlockchainAPIWeb.AccountController do
   use BlockchainAPIWeb, :controller
 
-  alias BlockchainAPI.{Util, DBManager}
+  alias BlockchainAPI.{Util, DBManager, Schema.Account}
   require Logger
 
   action_fallback BlockchainAPIWeb.FallbackController
@@ -20,9 +20,11 @@ defmodule BlockchainAPIWeb.AccountController do
   end
 
   def show(conn, %{"address" => address}) do
-    account = address
-              |> Util.string_to_bin()
-              |> DBManager.get_account!()
-    render(conn, "show.json", account: account)
+    bin_address = address |> Util.string_to_bin()
+    account = bin_address |> DBManager.get_account!() |> Account.encode_model()
+    account_balance_history = bin_address |> DBManager.get_account_balance_history()
+    account_with_balance = Map.merge(account, %{history: account_balance_history})
+
+    render(conn, "show.json", account: account_with_balance)
   end
 end
