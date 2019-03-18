500e1c5cdbde320602d471540339cced4e0f6301
Rahul Garg <vihu89@gmail.com>
2019-03-18 16:05:53 -0700

    Test pending, cleared txns thru one accnt route

--- lib/blockchain_api_web/controllers/account_transaction_controller.ex
+++ lib/blockchain_api_web/controllers/account_transaction_controller.ex
@@ -8,13 +8,22 @@ defmodule BlockchainAPIWeb.AccountTransactionController do
 
   def index(conn, %{"account_address" => address}=params) do
 
-    page = address
-           |> Util.string_to_bin()
-           |> Query.AccountTransaction.get(params)
+    bin_address = address |> Util.string_to_bin()
+
+    page = bin_address |> Query.AccountTransaction.get(params)
+    account_txns = page.entries
+
+    # If txn has already appeared in account_txns then remove it from pending_transactions list
+    pending_txns =
+      bin_address
+      |> Query.Account.get_pending_transactions()
+      |> Enum.reject(fn p -> Enum.any?(account_txns, fn t -> t.hash == p.hash end) end)
+
+    total_txns = pending_txns ++ account_txns
 
     render(conn,
       "index.json",
-      account_transactions: page.entries,
+      account_transactions: total_txns,
       page_number: page.page_number,
       page_size: page.page_size,
       total_pages: page.total_pages,
