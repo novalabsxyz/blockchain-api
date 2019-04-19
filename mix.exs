a239a14a1e6abe46cfb2dac46a66fd353393a062
Rahul Garg <vihu89@gmail.com>
2019-04-18 19:40:32 -0700

    Bump deps

--- mix.exs
+++ mix.exs
@@ -62,7 +62,7 @@ defmodule BlockchainAPI.MixProject do
 
       # blockchain requirements
       {:distillery, "~> 2.0"},
-      {:blockchain, git: "git@github.com:helium/blockchain-core.git", branch: "rg/txn-mgr-sup"},
+      {:blockchain, git: "git@github.com:helium/blockchain-core.git", branch: "master"},
       {:cuttlefish, git: "https://github.com/helium/cuttlefish.git", branch: "develop", override: true},
       {:h3, git: "https://github.com/helium/erlang-h3.git", branch: "master"},
       {:cors_plug, "~> 2.0"},
