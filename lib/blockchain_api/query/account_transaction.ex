69e521f425533b779e091bd6a03c85aa6374ab07
Rahul Garg <vihu89@gmail.com>
2019-04-27 15:04:57 -0700

    Update account transaction query

--- lib/blockchain_api/query/account_transaction.ex
+++ lib/blockchain_api/query/account_transaction.ex
@@ -25,51 +25,44 @@ defmodule BlockchainAPI.Query.AccountTransaction do
   def list(address, %{"before" => before, "limit" => limit}=_params) do
 
     # NOTE: no pagination for pending recvs
-    payee_pending = Query.PendingPayment.get_payee_pending(address)
+    pending = Query.PendingPayment.get_pending_by_address(address)
     rest =  address
             |> list_query()
             |> filter_before(before, limit)
             |> Repo.all()
             |> format()
 
-    payee_pending ++ rest
-
+    pending ++ rest
   end
   def list(address, %{"before" => before}=_params) do
-
-    # NOTE: no pagination for pending recvs
-    payee_pending = Query.PendingPayment.get_payee_pending(address)
+    pending = Query.PendingPayment.get_pending_by_address(address)
     rest = address
            |> list_query()
            |> filter_before(before, @default_limit)
            |> Repo.all()
            |> format()
 
-    payee_pending ++ rest
+    pending ++ rest
   end
   def list(address, %{"limit" => limit}=_params) do
-
-    # NOTE: no pagination for pending recvs
-    payee_pending = Query.PendingPayment.get_payee_pending(address)
+    pending = Query.PendingPayment.get_pending_by_address(address)
     rest = address
            |> list_query()
            |> limit(^limit)
            |> Repo.all()
            |> format()
 
-    payee_pending ++ rest
+    pending ++ rest
   end
   def list(address, %{}) do
-
-    # NOTE: no pagination for pending recvs
-    payee_pending = Query.PendingPayment.get_payee_pending(address)
+    pending = Query.PendingPayment.get_pending_by_address(address)
 
     rest = address
            |> list_query()
            |> Repo.all()
            |> format()
 
-    payee_pending ++ rest
+    pending ++ rest
   end
 
   def get_pending_txn!(txn_hash) do
