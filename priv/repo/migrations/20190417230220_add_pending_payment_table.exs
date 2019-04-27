b4a9855af69c9fd9b6eafe93afd341ad538ceb33
Rahul Garg <vihu89@gmail.com>
2019-04-27 15:08:00 -0700

    Add honeydew fields in pending_payment migration

--- priv/repo/migrations/20190417230220_add_pending_payment_table.exs
+++ priv/repo/migrations/20190417230220_add_pending_payment_table.exs
@@ -1,5 +1,7 @@
 defmodule BlockchainAPI.Repo.Migrations.AddPendingPaymentTable do
   use Ecto.Migration
+  import Honeydew.EctoPollQueue.Migration
+  import BlockchainAPI.Schema.PendingPayment, only: [submit_payment_queue: 0]
 
   def up do
     create table(:pending_payments) do
@@ -8,14 +10,18 @@ defmodule BlockchainAPI.Repo.Migrations.AddPendingPaymentTable do
       add :payee, :binary, null: false
       add :fee, :bigint, null: false, default: 0
       add :amount, :bigint, null: false
+      add :hash, :binary, null: false
+      add :txn, :binary, null: false
 
       add :payer, references(:accounts, on_delete: :nothing, column: :address, type: :binary), null: false
-      add :pending_transactions_hash, references(:pending_transactions, on_delete: :delete_all, column: :hash, type: :binary), null: false
+
+      honeydew_fields(submit_payment_queue())
 
       timestamps()
     end
 
-    create unique_index(:pending_payments, [:payer, :pending_transactions_hash, :status], name: :unique_pending_payment)
+    create unique_index(:pending_payments, [:payer, :hash, :status], name: :unique_pending_payment)
+    honeydew_indexes(:pending_payments, submit_payment_queue())
   end
 
   def down do
