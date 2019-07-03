b8384f0a11e5246a7a3d16e298be1b28a1bf5db8
Rahul Garg <vihu89@gmail.com>
2019-07-03 10:25:34 -0700

    Unique account txn constraint is no longer valid

--- lib/blockchain_api/schema/account_transaction.ex
+++ lib/blockchain_api/schema/account_transaction.ex
@@ -19,7 +19,7 @@ defmodule BlockchainAPI.Schema.AccountTransaction do
     account_transaction
     |> cast(attrs, [:account_address, :txn_hash, :txn_type, :txn_status])
     |> validate_required([:account_address, :txn_hash, :txn_type, :txn_status])
-    |> unique_constraint(:unique_account_txn, name: :unique_account_txn)
+    # |> unique_constraint(:unique_account_txn, name: :unique_account_txn)
   end
 
   def encode_model(account_transaction) do
