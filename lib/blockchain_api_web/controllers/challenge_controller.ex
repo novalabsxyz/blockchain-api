8711eff1e1c5c239fa8b63592d81548e1d5cb928
Chad Brading <chad@helium.com>
2019-07-26 14:51:47 -0700

    Refactor challenges api, remove 24 hours query

--- lib/blockchain_api_web/controllers/challenge_controller.ex
+++ lib/blockchain_api_web/controllers/challenge_controller.ex
@@ -1,22 +1,29 @@
 defmodule BlockchainAPIWeb.ChallengeController do
   use BlockchainAPIWeb, :controller
 
-  alias BlockchainAPI.Query
+  alias BlockchainAPI.{Query, Util}
 
   action_fallback BlockchainAPIWeb.FallbackController
 
   def index(conn, params) do
     challenges = Query.POCReceiptsTransaction.challenges(params)
     ongoing = Query.POCRequestTransaction.ongoing(params)
-    aggregated = Query.POCReceiptsTransaction.challenges_twenty_four_hrs(params)
 
-    {issued, successful, failed} =
-      case aggregated do
-        map when map_size(map) > 0 ->
-          {map.issued, map.successful, map.failed}
-        _ ->
-          {0, 0, 0}
-      end
+    start = Timex.now() |> Timex.shift(hours: -24) |> Timex.to_unix()
+    finish = Util.current_time()
+    {successful, failed} =
+      challenges
+      |> Enum.filter(fn challenge ->
+        challenge.time >= start && challenge.time <= finish
+      end)
+      |> Enum.reduce({0, 0}, fn c, {successful, failed} ->
+        if c.success == true do
+          {successful + 1, failed}
+        else
+          {successful, failed + 1}
+        end
+      end)
+    issued = successful + failed
 
     render(conn,
       "index.json",
