4156997cfddde3930282d0e8375a9a059a49c1aa
Rahul Garg <vihu89@gmail.com>
2019-07-28 12:37:29 -0700

    Update insertion of account
    - Consolidate dc, securities and balance
    - This got a bit screwed cuz of the formatter

--- lib/blockchain_api/schema/account.ex
+++ lib/blockchain_api/schema/account.ex
@@ -2,16 +2,32 @@ defmodule BlockchainAPI.Schema.Account do
   use Ecto.Schema
   import Ecto.Changeset
   alias BlockchainAPI.{Util, Schema.Account}
-  @fields [:id, :address, :name, :balance, :fee, :nonce]
+
+  @fields [
+    :id,
+    :address,
+    :name,
+    :balance,
+    :dc_balance,
+    :security_balance,
+    :fee,
+    :nonce,
+    :dc_nonce,
+    :security_nonce
+  ]
 
   @derive {Phoenix.Param, key: :address}
   @derive {Jason.Encoder, only: @fields}
   schema "accounts" do
     field :name, :string, null: true
-    field :balance, :integer, null: false
     field :address, :binary, null: false
+    field :balance, :integer, null: false, default: 0
+    field :dc_balance, :integer, null: false, default: 0
+    field :security_balance, :integer, null: false, default: 0
     field :fee, :integer, null: false, default: 0
     field :nonce, :integer, null: false, default: 0
+    field :dc_nonce, :integer, null: false, default: 0
+    field :security_nonce, :integer, null: false, default: 0
 
     timestamps()
   end
@@ -19,15 +35,25 @@ defmodule BlockchainAPI.Schema.Account do
   @doc false
   def changeset(account, attrs) do
     account
-    |> cast(attrs, [:address, :name, :balance, :fee, :nonce])
+    |> cast(attrs, [
+      :address,
+      :name,
+      :balance,
+      :dc_balance,
+      :security_balance,
+      :fee,
+      :nonce,
+      :dc_nonce,
+      :security_nonce
+    ])
     |> validate_required([:address, :balance, :fee, :nonce])
     |> unique_constraint(:address)
   end
 
   def encode_model(account) do
     %{
-      Map.take(account, @fields) |
-      address: Util.bin_to_string(account.address)
+      Map.take(account, @fields)
+      | address: Util.bin_to_string(account.address)
     }
   end
 
--- priv/repo/migrations/20190122051731_add_account_table.exs
+++ priv/repo/migrations/20190122051731_add_account_table.exs
@@ -6,13 +6,16 @@ defmodule BlockchainAPI.Repo.Migrations.AddAccountTable do
       add :address, :binary, null: false
       add :name, :string
       add :balance, :bigint, null: false
+      add :dc_balance, :bigint, null: false, default: 0
+      add :security_balance, :bigint, null: false, default: 0
       add :fee, :bigint, null: false, default: 0
       add :nonce, :bigint, null: false, default: 0
+      add :dc_nonce, :bigint, null: false, default: 0
+      add :security_nonce, :bigint, null: false, default: 0
 
       timestamps()
     end
 
     create unique_index(:accounts, [:address], name: :unique_account_address)
   end
-
 end
