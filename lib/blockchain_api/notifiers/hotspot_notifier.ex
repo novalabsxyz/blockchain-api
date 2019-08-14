12b2af3310d7f7cc52e3b0d47734726780694c39
Chad Brading <chad@helium.com>
2019-08-14 14:58:59 -0700

    Send notification when new hotspot is added

---
+++ lib/blockchain_api/notifiers/hotspot_notifier.ex
@@ -0,0 +1,19 @@
+defmodule BlockchainAPI.HotspotNotifier do
+  alias BlockchainAPI.{NotifierClient, Schema.Hotspot}
+
+  def send_new_hotspot_notification(txn, type, ledger) do
+     map = Hotspot.map(type, txn, ledger)
+    NotifierClient.post(data(map), message())
+  end
+
+  defp data(hotspot) do
+    %{
+      address: Util.bin_to_string(hotspot.address),
+      owner: Util.bin_to_string(hotspot.owner)
+    }
+  end
+
+  defp message do
+    "Your hotspot has been added to the network!"
+  end
+end
