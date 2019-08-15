e11f7c93f35ea1a5b02582b190725de5678cacbb
Chad Brading <chad@helium.com>
2019-08-15 11:58:42 -0700

    Add test for issued challenges

---
+++ test/blockchain_api/query/poc_receipts_transaction.exs
@@ -0,0 +1,15 @@
+defmodule BlockchainAPI.Query.POCReceiptsTransactionTest do
+  use BlockchainAPI.DataCase
+  import BlockchainAPI.TestHelpers
+
+  alias BlockchainAPI.Query.POCReiptsTransaction
+
+  setup do
+    insert_fake_challenges()
+    :ok
+  end
+
+  test "returns challenges issued" do
+    assert POCReceiptsTransaction.issued() == 1000
+  end
+end
