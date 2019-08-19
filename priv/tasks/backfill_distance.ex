3b8299b9eab73581fb9ad6597c291b9264ec55bb
Andrew Allen <allenan@helium.com>
2019-08-19 11:24:35 -0700

    add backfill distances mix task

---
+++ priv/tasks/backfill_distance.ex
@@ -0,0 +1,63 @@
+defmodule Mix.Tasks.BackfillDistance do
+  @start_apps [
+    :postgrex,
+    :ecto,
+    :ecto_sql,
+  ]
+
+  @repo BlockchainAPI.Repo
+
+  use Mix.Task
+
+  import Ecto.Query, warn: false
+
+  alias BlockchainAPI.{
+    Repo,
+    Schema.POCWitness,
+    Schema.POCPathElement,
+    Util
+  }
+
+  defp boot() do
+    IO.puts "Booting pre hook..."
+    # Ensure postgrex and ecto applications started
+    Enum.each(@start_apps, &Application.ensure_all_started/1)
+  end
+
+  defp start_connection() do
+    {:ok, _ } = @repo.start_link(pool_size: 10)
+  end
+
+  # Took ~20s to run locally on ~53k records
+  @shortdoc "Backfills poc_witnesses.distance field"
+  def run(_) do
+    boot()
+    start_connection()
+    IO.puts("Backfilling distance...")
+
+    query = from(
+      wx in POCWitness,
+      where: is_nil(wx.distance),
+      left_join: path_element in POCPathElement,
+      on: wx.poc_path_elements_id == path_element.id,
+      select: %{
+        wx_id: wx.id,
+        wx_loc: wx.location,
+        challengee_loc: path_element.challengee_loc
+      }
+    )
+
+    witnesses_without_distance = query |> Repo.all()
+
+    for %{wx_id: wx_id, wx_loc: wx_loc, challengee_loc: challengee_loc} <- witnesses_without_distance do
+      distance = Util.h3_distance_in_meters(wx_loc |> to_h3(), challengee_loc |> to_h3())
+      %POCWitness{id: wx_id}
+      |> POCWitness.changeset(%{distance: distance})
+      |> Repo.update()
+    end
+  end
+
+  def to_h3(string_index) do
+    string_index |> String.to_charlist() |> :h3.from_string()
+  end
+end
