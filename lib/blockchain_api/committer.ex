cbfb05e5573667a16f07cef1313b677d73c673a1
Rahul Garg <vihu89@gmail.com>
2019-08-26 21:31:35 -0700

    Fix sync flag notification case

--- lib/blockchain_api/committer.ex
+++ lib/blockchain_api/committer.ex
@@ -35,7 +35,13 @@ defmodule BlockchainAPI.Committer do
   def commit(block, ledger, height, sync_flag) do
     case commit_block(block, ledger, height) do
       {:ok, term} ->
-        if !sync_flag, do: Notifier.notify(block, ledger)
+        :ok =
+          case sync_flag do
+            false ->
+              :ok
+            true ->
+              Notifier.notify(block, ledger)
+          end
         Logger.info("Success! Commit block: #{height}")
         {:ok, term}
       {:error, reason} ->
