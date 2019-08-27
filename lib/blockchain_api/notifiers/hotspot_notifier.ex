0539e1185fa5d548edf60e69ac7467fecd3dc0a6
Rahul Garg <vihu89@gmail.com>
2019-08-26 21:32:45 -0700

    Fix notifier client post call

--- lib/blockchain_api/notifiers/hotspot_notifier.ex
+++ lib/blockchain_api/notifiers/hotspot_notifier.ex
@@ -3,18 +3,28 @@ defmodule BlockchainAPI.HotspotNotifier do
 
   def send_new_hotspot_notification(txn, type, ledger) do
     map = Hotspot.map(type, txn, ledger)
-    NotifierClient.post(data(map), message(map))
+    data = %{address: Util.bin_to_string(map.address), owner: Util.bin_to_string(map.owner)}
+    animal_name = Hotspot.animal_name(map.address)
+    message = "#{animal_name} has been added to the network!"
+    @notifier_client.post(data, message, data.address)
   end
 
-  defp data(hotspot) do
-    %{
-      address: Util.bin_to_string(hotspot.address),
-      owner: Util.bin_to_string(hotspot.owner)
-    }
+  def send_add_hotspot_failed(:timed_out, pending_gateway) do
+    data = %{gateway: pending_gateway.gateway, owner: pending_gateway.owner}
+    message = "Unable to Add Hotspot. Transaction Timed Out."
+    @notifier_client.post(data, message, data.address)
   end
 
-  defp message(hotspot) do
-    animal_name = Hotspot.animal_name(hotspot.address)
-    "#{animal_name} has been added to the network!"
+  def send_add_hotspot_failed(:already_exists, pending_gateway) do
+    data = %{gateway: pending_gateway.gateway, owner: pending_gateway.owner}
+    message = "Unable to Add Hotspot. Hotspot Already on Blockchain."
+    @notifier_client.post(data, message, data.address)
+  end
+
+  def send_confirm_location_failed(pending_location) do
+    data = %{gateway: pending_location.gateway, owner: pending_location.owner}
+    animal_name = Hotspot.animal_name(pending_location)
+    message = "#{animal_name} Added Without Location Information."
+    @notifier_client.post(data, message, data.address)
   end
 end
