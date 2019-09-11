8cc59aba4677347491a2a8ebdbb3d0b0a198a124
Chad Brading <chad@helium.com>
2019-09-11 13:39:22 -0700

    Refactor units Util code

--- lib/blockchain_api/notifiers/payments_notifier.ex
+++ lib/blockchain_api/notifiers/payments_notifier.ex
@@ -1,5 +1,4 @@
 defmodule BlockchainAPI.PaymentsNotifier do
-  @bones 100000000
   @ticker "HLM"
 
   alias BlockchainAPI.Util
@@ -7,7 +6,7 @@ defmodule BlockchainAPI.PaymentsNotifier do
 
   def send_notification(txn) do
     amount = :blockchain_txn_payment_v1.amount(txn)
-    msg = amount |> units() |> message()
+    msg = amount |> Util.units() |> message()
     data = payment_data(txn, amount)
     @notifier_client.post(data, msg, data.address)
   end
@@ -23,30 +22,6 @@ defmodule BlockchainAPI.PaymentsNotifier do
 
   defp message(units) do
     "You got #{units} #{@ticker}"
-  end
-
-  def units(amount) when is_integer(amount) do
-    amount |> Decimal.div(@bones) |> delimit_unit()
-  end
-  def units(amount) when is_float(amount) do
-    amount |> Decimal.from_float() |> Decimal.div(@bones) |> delimit_unit()
-  end
-
-  defp delimit_unit(units0) do
-    unit_str = units0 |> Decimal.to_string()
-    case :binary.match(unit_str, ".") do
-      {start, _} ->
-        precision = byte_size(unit_str) - start - 1
-        units0
-        |> Decimal.to_float()
-        |> Number.Delimit.number_to_delimited(precision: precision)
-        |> String.trim_trailing("0")
-
-      :nomatch ->
-        units0
-        |> Decimal.to_float()
-        |> Number.Delimit.number_to_delimited(precision: 0)
-    end
   end
 
 end
