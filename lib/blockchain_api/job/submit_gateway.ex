49c279a5cb5d08147d4ca6df80374ac72fe7d86f
Rahul Garg <vihu89@gmail.com>
2019-09-17 11:05:31 -0700

    Blacklist known owner submitting txns

--- lib/blockchain_api/job/submit_gateway.ex
+++ lib/blockchain_api/job/submit_gateway.ex
@@ -3,31 +3,40 @@ defmodule BlockchainAPI.Job.SubmitGateway do
   alias BlockchainAPI.Util
   require Logger
 
+  @blacklisted_owner BlockchainAPI.Util.string_to_bin("14CJX5YCRf94kbhL9PPn58SL9EzqUGsrALeaEar4UikM4EB3Mx7")
+
   def run(id) do
     Logger.debug("running pending_gateway job: #{inspect(id)}")
 
     pending_gateway = PendingGateway.get_by_id!(id)
     txn = pending_gateway.txn |> :blockchain_txn.deserialize()
 
-    txn
-    |> :blockchain_worker.submit_txn(fn res ->
-      case res do
-        :ok ->
-          Logger.info("Txn: #{Util.bin_to_string(:blockchain_txn.hash(txn))} accepted!")
+    case :blockchain_txn_add_gateway_v1.owner(txn) do
+      @blacklisted_owner ->
+        Logger.warn("You are blacklisted! #{inspect(txn)}")
+      _ ->
 
-          pending_gateway
-          |> PendingGateway.update!(%{status: "cleared"})
+        txn
+        |> :blockchain_worker.submit_txn(fn res ->
+          case res do
+            :ok ->
+              Logger.info("Txn: #{Util.bin_to_string(:blockchain_txn.hash(txn))} accepted!")
 
-        {:error, reason} ->
-          Logger.error(
-            "Txn: #{Util.bin_to_string(:blockchain_txn.hash(txn))} failed!, reason: #{
-              inspect(reason)
-            }"
-          )
+              pending_gateway
+              |> PendingGateway.update!(%{status: "cleared"})
 
-          pending_gateway
-          |> PendingGateway.update!(%{status: "error"})
-      end
-    end)
+            {:error, reason} ->
+              Logger.error(
+                "Txn: #{Util.bin_to_string(:blockchain_txn.hash(txn))} failed!, reason: #{
+                  inspect(reason)
+                }"
+                  )
+
+              pending_gateway
+              |> PendingGateway.update!(%{status: "error"})
+          end
+        end)
+    end
   end
+
 end
--- lib/blockchain_api/job/submit_location.ex
+++ lib/blockchain_api/job/submit_location.ex
@@ -3,31 +3,39 @@ defmodule BlockchainAPI.Job.SubmitLocation do
   alias BlockchainAPI.Util
   require Logger
 
+  @blacklisted_owner BlockchainAPI.Util.string_to_bin("14CJX5YCRf94kbhL9PPn58SL9EzqUGsrALeaEar4UikM4EB3Mx7")
+
   def run(id) do
     Logger.debug("running pending_location job: #{inspect(id)}")
 
     pending_location = PendingLocation.get_by_id!(id)
     txn = pending_location.txn |> :blockchain_txn.deserialize()
 
-    txn
-    |> :blockchain_worker.submit_txn(fn res ->
-      case res do
-        :ok ->
-          Logger.info("Txn: #{Util.bin_to_string(:blockchain_txn.hash(txn))} accepted!")
+    case :blockchain_txn_assert_location_v1.owner(txn) do
+      @blacklisted_owner ->
+        Logger.warn("You are blacklisted! #{inspect(txn)}")
+      _ ->
 
-          pending_location
-          |> PendingLocation.update!(%{status: "cleared"})
+        txn
+        |> :blockchain_worker.submit_txn(fn res ->
+          case res do
+            :ok ->
+              Logger.info("Txn: #{Util.bin_to_string(:blockchain_txn.hash(txn))} accepted!")
 
-        {:error, reason} ->
-          Logger.error(
-            "Txn: #{Util.bin_to_string(:blockchain_txn.hash(txn))} failed!, reason: #{
-              inspect(reason)
-            }"
-          )
+              pending_location
+              |> PendingLocation.update!(%{status: "cleared"})
 
-          pending_location
-          |> PendingLocation.update!(%{status: "error"})
-      end
-    end)
+            {:error, reason} ->
+              Logger.error(
+                "Txn: #{Util.bin_to_string(:blockchain_txn.hash(txn))} failed!, reason: #{
+                  inspect(reason)
+                }"
+              )
+
+              pending_location
+              |> PendingLocation.update!(%{status: "error"})
+          end
+        end)
+    end
   end
 end
