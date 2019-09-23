ed0cd85e726280c6a022231024ec2a53fb964df0
Rahul Garg <vihu89@gmail.com>
2019-09-23 12:04:56 -0700

    Switch to appsignal's master brnch instead of fork

--- mix.exs
+++ mix.exs
@@ -68,7 +68,8 @@ defmodule BlockchainAPI.MixProject do
       {:distillery, "~> 2.0"},
       {:cors_plug, "~> 2.0"},
       {:logger_file_backend, "~> 0.0.10"},
-      {:appsignal, git: "git@github.com:vihu/appsignal-elixir.git", branch: "rg/hackney-workaround"},
+      {:appsignal,
+        git: "https://github.com/appsignal/appsignal-elixir", branch: "master"},
 
       # blockchain requirements
       {:blockchain, git: "git@github.com:helium/blockchain-core.git", branch: "master"},
