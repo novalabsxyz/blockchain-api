a4bf40198c987e0f8659868a20ef707630689fbe
Rahul Garg <vihu89@gmail.com>
2019-10-22 16:25:44 -0700

    Add oui transaction migration

---
+++ priv/repo/migrations/20191022231530_add_oui_transactions_table.exs
@@ -0,0 +1,20 @@
+defmodule BlockchainAPI.Repo.Migrations.AddOuiTransactionsTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:oui_transactions) do
+      add :owner, :binary, null: false
+      add :payer, :binary, null: false, default: "" # can be empty payer
+      add :addresses, {:array, :binary}, null: false, default: [] # can have empty addresses
+      add :fee, :bigint, null: false, default: 0
+      add :staking_fee, :bigint, null: false, default: 0
+      add :status, :string, null: false, default: "cleared"
+
+      add :hash, references(:transactions, on_delete: :nothing, column: :hash, type: :binary), null: false
+      timestamps()
+    end
+
+    create unique_index(:oui_transactions, [:hash], name: :unique_oui_hash)
+  end
+
+end
