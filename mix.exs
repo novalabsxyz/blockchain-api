5a6f7962c2a9c1c6c81abd2dba4fb4a2b7616a86
Rahul Garg <vihu89@gmail.com>
2019-10-25 13:45:34 -0700

    Add exmachina test dep

--- mix.exs
+++ mix.exs
@@ -72,6 +72,7 @@ defmodule BlockchainAPI.MixProject do
       {:appsignal,
         git: "https://github.com/appsignal/appsignal-elixir", branch: "master"},
       {:uuid, "~> 1.1"},
+      {:ex_machina, "~> 2.3", only: :test},
 
       # blockchain requirements
       {:blockchain, git: "git@github.com:helium/blockchain-core.git", branch: "master"},
