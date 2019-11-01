7d57455df717fbfc88d0f62bc36cdcc84fd39f8b
Rahul Garg <vihu89@gmail.com>
2019-11-01 10:15:37 -0700

    Query for account rewards

--- lib/blockchain_api/query/rewards_transaction.ex
+++ lib/blockchain_api/query/rewards_transaction.ex
@@ -2,7 +2,12 @@ defmodule BlockchainAPI.Query.RewardsTransaction do
   @moduledoc false
   import Ecto.Query, warn: false
 
-  alias BlockchainAPI.{Repo, Schema.RewardsTransaction}
+  alias BlockchainAPI.{
+    Repo,
+    Schema.RewardsTransaction,
+    Schema.RewardTxn,
+    Schema.Transaction
+  }
 
   def create(attrs \\ %{}) do
     %RewardsTransaction{}
@@ -20,4 +25,26 @@ defmodule BlockchainAPI.Query.RewardsTransaction do
   def list(_params) do
     from(r in RewardsTransaction, preload: [:reward_txns]) |> Repo.all()
   end
+
+  def list_for(account_addr) do
+    from(
+      r in RewardsTransaction,
+      left_join: rt in RewardTxn,
+      on: r.hash == rt.rewards_hash,
+      left_join: t in Transaction,
+      on: r.hash == t.hash,
+      where: rt.account == ^account_addr,
+      order_by: [desc: rt.id],
+      select: %{
+        hotspot: rt.gateway,
+        account: rt.account,
+        amount: rt.amount,
+        type: rt.type,
+        hash: rt.hash,
+        height: t.block_height
+      }
+    )
+    |> Repo.all()
+  end
+
 end
