fea9fe8140956e1eb8493094393812a132e7bfc4
Rahul Garg <vihu89@gmail.com>
2019-11-05 12:19:52 -0800

    Switch witness route to use hotspot address

--- lib/blockchain_api_web/router.ex
+++ lib/blockchain_api_web/router.ex
@@ -52,7 +52,7 @@ defmodule BlockchainAPIWeb.Router do
 
     get "/stats", StatsController, :show
 
-    resources "/witnesses", WitnessController, only: [:show], param: "name"
+    resources "/witnesses", WitnessController, only: [:show], param: "address"
   end
 
   scope "/", BlockchainAPIWeb do
